63ab232dd6ecc2dc664efc4400205e25698312b5
Sonal-BP <108402618+Sonal-BP@users.noreply.github.com>
2024-08-10 18:04:57 +0530

    Update install.groovy

--- install.groovy
+++ install.groovy
@@ -1,9 +1,10 @@
 pipeline {
     agent any
     environment {
-        // Define environment variables here if needed
+        // AWS credentials (Assuming they're needed in some part of your pipeline)
         AWS_ACCESS_KEY_ID     = credentials('aws-access-key-id')
         AWS_SECRET_ACCESS_KEY = credentials('aws-secret-access-key')
+        // Define SSH credentials ID here for reuse
         SSH_KEY_ID            = 'ssh-credentials-id'
     }
     stages {
@@ -13,33 +14,38 @@ pipeline {
                 git branch: 'main', url: 'https://github.com/Sonal-BP/jekinstasks.git'
             }
         }
+        stage('Setup SSH Known Hosts') {
+            steps {
+                // Create .ssh directory if it does not exist
+                sh '''
+                mkdir -p ~/.ssh
+                ssh-keyscan -H 172.31.22.71 >> ~/.ssh/known_hosts
+                ssh-keyscan -H 172.31.17.0 >> ~/.ssh/known_hosts
+                ssh-keyscan -H 172.31.46.193 >> ~/.ssh/known_hosts
+                '''
+            }
+        }
         stage('Install Jenkins') {
             steps {
-                script {
-                    // Use sshagent to execute commands on remote servers
-                     sshagent([env.SSH_KEY_ID]) {
-                         sh 'ansible-playbook nexusinstall.yaml -i inventory.ini'
-                    }
+                sshagent([env.SSH_KEY_ID]) {
+                    // Install Jenkins
+                    sh 'ansible-playbook jenkinsinstall.yaml -i inventory.ini'
                 }
             }
         }
         stage('Install Nexus') {
             steps {
-                script {
-                    // Use sshagent to execute commands on remote servers
-                    sshagent(['ssh-credentials-id']) {
-                        sh 'ansible-playbook nexusinstall.yaml -i inventory.ini'
-                    }
+                sshagent([env.SSH_KEY_ID]) {
+                    // Install Nexus Repository Manager
+                    sh 'ansible-playbook nexusinstall.yaml -i inventory.ini'
                 }
             }
         }
         stage('Install MicroK8s') {
             steps {
-                script {
-                    // Use sshagent to execute commands on remote servers
-                    sshagent(['ssh-credentials-id']) {
-                        sh 'ansible-playbook microk8s.yaml -i inventory.ini'
-                    }
+                sshagent([env.SSH_KEY_ID]) {
+                    // Install MicroK8s
+                    sh 'ansible-playbook microk8s.yaml -i inventory.ini'
                 }
             }
         }
